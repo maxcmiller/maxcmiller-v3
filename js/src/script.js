a0baa7a4e86e081369f2cd67fdfeb7fc4d17792f
einkelflugle <cluckyferret@gmail.com>
2015-09-29 18:37:23 +1000

    Adds lightbox and additional portfolio images

---
+++ js/src/script.js
@@ -0,0 +1,67 @@
+var scroll
+var $hero, $heroContent, $skills, $skillsList
+var direction = {
+	x: 'x',
+	y: 'y'
+}
+
+$(document).ready(function() {
+	$hero = $('.hero')
+	$heroContent = $('.hero__content')
+	$skills = $('.skills')
+	$skillsList = $('.skills__list')
+
+	$(window).scroll(function() {
+		scroll = $(this).scrollTop()
+		window.requestAnimationFrame(scrollHandler)
+	})
+
+	/*
+		Following code thanks Chris Coyier from CSS-Tricks.com
+		*/
+	$('a[href*=#]:not([href=#])').click(function() {
+	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	    var target = $(this.hash);
+	    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	    if (target.length) {
+	      $('html,body').animate({
+	        scrollTop: target.offset().top
+	      }, 1000);
+	      return false;
+	    }
+	  }
+	});
+})
+
+function scrollHandler() {
+	// Hero animation
+	heroHeight = $hero.height()
+	if (scroll < heroHeight) {
+		applyTranslate($heroContent, scroll / 3, direction.y)
+	}
+	applyFade($heroContent, scroll / heroHeight)
+
+	// Stars animation
+	if (window.innerHeight + scroll >= $skills.offset().top + $skills.height()) {
+		$skillsList.find('.skills__list__stars__star').each(function(i) {
+			var $star = $(this)
+			setTimeout(function() {
+				$star.addClass('fadeInRight')
+				$star.removeClass('hidden')
+			}, 50 * i)
+		})
+	}
+}
+
+function applyTranslate($element, factor, direction) {
+	$element.css({
+		'-webkit-transform': 'translate' + direction.toUpperCase() + '(' + factor + 'px)',
+		'transform': 'translate' + direction.toUpperCase() + '(' + factor + 'px)',
+	})
+}
+
+function applyFade($element, factor) {
+	$element.css({
+		'opacity': 1 - factor
+	})
+}
